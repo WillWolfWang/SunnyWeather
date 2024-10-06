20368ed991429ec3294b7a8bb4cb3f30afdcd35a
wangzhenhua <WillWolf_Wang@126.com>
2024-10-06 13:37:04 +0800

    添加天气预报的 viewModel

---
+++ app/src/main/java/com/will/sunnyweather/ui/place/WeatherViewModel.kt
@@ -0,0 +1,26 @@
+package com.will.sunnyweather.ui.place
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.will.sunnyweather.logic.Repository
+import com.will.sunnyweather.logic.model.Location
+import com.will.sunnyweather.logic.model.Weather
+
+class WeatherViewModel: ViewModel() {
+    var locationLng = ""
+    var locationLat = ""
+    var placeName = ""
+
+    // 定义一个查询天气的 liveData
+    private val locationLiveData = MutableLiveData<Location>()
+    val weatherLiveData: LiveData<Result<Weather>> = locationLiveData.switchMap { location->
+        Repository.searchWeather(location.lat, location.lng)
+    }
+    fun searchWeather(lat: String, lng: String) {
+        locationLiveData.value = Location(lat, lng)
+    }
+
+
+}
