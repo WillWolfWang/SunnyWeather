cd6407ae4ce3ff9ffa27a982225a55fc4cd421a8
wangzhenhua <WillWolf_Wang@126.com>
2024-10-06 13:32:51 +0800

    添加获取实时天气和未来天气的方法

--- app/src/main/java/com/will/sunnyweather/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/will/sunnyweather/logic/network/SunnyWeatherNetwork.kt
@@ -1,6 +1,9 @@
 package com.will.sunnyweather.logic.network
 
+import android.util.Log
+import com.will.sunnyweather.logic.model.DailyResponse
 import com.will.sunnyweather.logic.model.PlaceResponse
+import com.will.sunnyweather.logic.model.RealtimeResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,11 +18,22 @@ object SunnyWeatherNetwork {
 
     // 这里执行 retrofit 的请求方法调用
     // 获取 PlaceService
-    private var placeService = ServiceCreator.createApiService(PlaceService::class.java)
+    private val placeService = ServiceCreator.createApiService(PlaceService::class.java)
+
+    // 获取 WeatherService
+    private val weatherService = ServiceCreator.create<WeatherService>()
 
     // 通过使用扩展函数，可以直接得到结果
     suspend fun searchPlace(city: String): PlaceResponse{
        return placeService.searchPlace(city).await<PlaceResponse>()
+    }
+
+    suspend fun searchRealtimeWeather(lat: String, lng: String): RealtimeResponse {
+        return weatherService.searchRealtimeWeather(lat, lng).await<RealtimeResponse>()
+    }
+
+    suspend fun searchDailyWeather(lat: String, lng: String): DailyResponse {
+        return weatherService.searchDailyWeather(lat, lng).await<DailyResponse>()
     }
 
     // 给 call 定义一个扩展函数，并且使用 SuspendCoroutine 将回调进行包装
